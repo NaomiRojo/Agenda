f9b680d93475d922233d41142ba9eb3292c4b2fd
NaomiRojo <andre.rojo.m@gmail.com>
2024-10-23 10:36:22 -0400

    metodo post

--- api.py
+++ api.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 opciones = OpcionesEjecutar()#lee los metodos
 
 #Muestra las tareas
-@app.route('/tareas', methods=['GET'])
+@app.route('/tareas', methods=['GET'])#definmos ruta
 def ObtenerTareas():
-    return jsonify([{'id': tarea.id, 'titulo': tarea.titulo, 'completada': tarea.completada} for tarea in opciones.tareas])
+    return jsonify([{'id': tarea.id, 'titulo': tarea.titulo, 'completada': tarea.completada} for tarea in opciones.tareas])#devuelve lista de tareas
+
+
+#añadir tarea
+@app.route('/tareas', methods=['POST'])
+def NuevaTarea():
+    datos = request.get_json()#obtiene la peticion
+    nuevatarea = Tarea(len(opciones.tareas) + 1, data['titulo'])
+    opciones.NuevaTarea(datos['titulo'])#se crea y añade la tarea
+    return jsonify({'mensaje': 'Añadida', 'id': nuevatarea.id}), 201
+
 
